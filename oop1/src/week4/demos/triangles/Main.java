80b4b14541ef2e641907668b05edb699b762ed22
Brad Bush <67883440+BBradBush@users.noreply.github.com>
2025-01-27 18:58:17 -0500

    Create Main.java (week4.demos.triangles)

---
+++ oop1/src/week4/demos/triangles/Main.java
@@ -0,0 +1,43 @@
+package week4.demos.triangles;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		displayHalfTriangle(8);
+		displayFullTriangle(8);
+	}
+	private static void displayHalfTriangle(int n)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (j < (n - i))
+				{
+					System.out.print(" ");
+				}
+				else
+				{
+					System.out.print("*");
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+	private static void displayFullTriangle(int n)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 0; j < (n - i); j++)
+			{
+				System.out.print(" ");
+			}
+			for (int j = 0; j < ((i * 2) - 1); j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+}
